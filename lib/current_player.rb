8e1c4cc36f3e62e81bc72a3cffc9538548698541
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-10-07 16:11:54 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,10 +6,9 @@ def turn_count(current_player)
   if current_player.even?== true
     puts "x"
     current_player+=1
-  end
   elsif current_player.even? == true
     puts "o"
-  end
 end
+
     
 turn_count(current_player)
