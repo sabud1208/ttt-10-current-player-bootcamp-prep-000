5648e964f692011151fedafb3c2d0a02a443f64b
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-10-07 16:12:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,12 +3,12 @@ board=[" ", " ", " ", " ", " ", " " , " ", " ", " "]
 
 def turn_count(current_player)
   current=1
-  if current_player.even?== true
+  if current_player.even?==true
     puts "x"
     current_player+=1
-  elsif current_player.even? == true
+  elsif current_player.even? ==true
     puts "o"
 end
 
-    
+
 turn_count(current_player)
